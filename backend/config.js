4d57f3c666170862a904e4a72122c139c93c8151
Stuart Myers <114456110+rileystuartmyers@users.noreply.github.com>
2025-07-13 13:34:38 -0500

    Upload Project

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5000;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:chichi123@cluster0.e2ec7n7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
