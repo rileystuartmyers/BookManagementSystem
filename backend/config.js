846bdfb8600f46c6ea38538d19df6e01e935af88
Stuart Myers <114456110+rileystuartmyers@users.noreply.github.com>
2025-07-13 14:13:07 -0500

    Removed config.js

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5000;
-
-export const mongoDBURL = 
-    'mongodb+srv://root:chichi123@cluster0.e2ec7n7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
